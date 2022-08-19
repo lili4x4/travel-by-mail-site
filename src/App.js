a4654c4cb2807eb997df644133179c95996b90bd
Liliana <liliana.m.parra@gmail.com>
2022-08-19 19:11:33 +0200

    debugging deployment

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="header-text">Under Construction</h1>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
