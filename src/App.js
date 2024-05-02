5810dde628bda97fd95ea1205a4d112e0aa9f1ca
Liliana <liliana.m.parra@gmail.com>
2024-05-02 11:59:10 -0400

    Travel By Mail coming soon splash page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import logo from './assets/TravByMail_Logo_Base.png'
+import logoCorner from './assets/TravByMail_Logo_GenericIcon.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="header-text">Under Construction</h1>
+        <h1 className="header-text">COMING SOON</h1>
+        <img className="logo-central" src={logo} alt="Travel By Mail" />
+        <img className="logo-corner" src={logoCorner} alt="stamp" />
       </header>
     </div>
   );
